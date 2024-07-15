099032f3e67176aa8635dc7fd8e509140c97900d
Faris <farissharayri@gmail.com>
2024-07-15 11:39:54 -0400

    redid the whole project with steps

--- js/app.js
+++ js/app.js
@@ -1,99 +1,116 @@
-
 /*-------------------------------- Constants --------------------------------*/
-const squareElements = document.querySelectorAll(".sqr")
-const boardElement = document.querySelector(".board")
-const messageElement= document.querySelector("#message")
-const buttonElement = document.querySelector("#button")
+const winningCombos =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 
 /*---------------------------- Variables (state) ----------------------------*/
-do{
-play = false
-let turnXO = 0
+let board 
+let turn
 let winner
+let tie
 
 
 
 /*------------------------ Cached Element References ------------------------*/
 
-
-
-
-
-
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
+const boardEl = document.querySelector('.board')
+const resetBtnEl = document.querySelector('#reset')
 /*-------------------------------- Functions --------------------------------*/
-markSquare = function(e){
-    let squareEl = e.target
-    const writeX = (element)=> {
-        element.textContent = "X"
-        element.classList.toggle("used")
-    }
-    const writeO = (element)=> {
-        element.textContent = "O"
-        element.classList.toggle("used")
-    }
-    if (squareEl.textContent ===''){
-        turnXO++
+function init(){
+    board = ['','','',
+            '','','',
+            '','','']
+    turn = 'X'
+    winner = false
+    tie = false
 
-        if (turnXO%2 ===1){
-            writeX(squareEl)
-            messageElement.textContent= "O player's turn"
-        }
-        else {
-            writeO(squareEl)
-            messageElement.textContent= "X player's turn"
-        }
-
-    }
-    if (winCondition()){
-       
-        messageElement.textContent= `player ${winner} Wins !!`
-        boardElement.removeEventListener('click',markSquare)
-        // againButtonElement.disabled = false
-    }
-    else if (turnXO===9){messageElement.textContent=" its a Tie"}
+    render()
     
 }
-function winCondition(){
-    for (let i =0; i<3; i++) {
-    if (squareElements[i].textContent===squareElements[i+3].textContent&& squareElements[i].textContent===squareElements[i+6].textContent&&squareElements[i].textContent!=='')
-        {winner = squareElements[i].textContent
-        return true}
+
+
+function render(){
+    updateBoard()
+    updateMessage()
+}
+
+function updateBoard(){
+    board.forEach((element,i) => {
+        squareEls[i].textContent = element 
     
-    }
-    for (let i= 0 ; i<7; i+=3){
-        if (squareElements[i].textContent===squareElements[i+1].textContent && squareElements[i].textContent===squareElements[i+2].textContent && squareElements[i].textContent!=='')
-            {winner = squareElements[i].textContent
-             return true}
-    }
-    if (squareElements[0].textContent===squareElements[4].textContent&& squareElements[0].textContent===squareElements[8].textContent&&squareElements[0].textContent!=='')
-        {winner = squareElements[0].textContent
-            return true}
-    if (squareElements[2].textContent===squareElements[4].textContent&& squareElements[2].textContent===squareElements[6].textContent&&squareElements[2].textContent!=='')
-        {winner = squareElements[2].textContent
-            return true}
-    return false
+        if (element==='')
+            squareEls[i].style.backgroundColor = 'white'
+        if (element==='X')
+            squareEls[i].style.backgroundColor = 'darkblue'
+        if (element==='O')
+            squareEls[i].style.backgroundColor = 'darkorange'
+    })
 
 }
-function playagain(){
-    // clear all squares 
-
-    for(squareElement of squareElements){
-        squareElement.textContent =''
-        squareElement.classList.remove("used")
+function updateMessage(){
+    if (!winner && !tie){
+        messageEl.textContent=`${turn}'s turn `
     }
-    // clickable squares 
-    boardElement.addEventListener('click', markSquare)
-    // new message
-    messageElement.textContent ="X's turn"
-    let play = true
-    turnXO= 0
-    
+    else if (tie && !winner){
+         messageEl.textContent='tie !'
+
+    }
+    else {
+         messageEl.textContent=`${turn} Wins!`
+    }
+
 }
+function handleClick(event){
+    if (event.target.className !=='sqr') return
+    let squareIndex = event.target.id
+    if (board[squareIndex] !== '') return
+    if (winner) return
+    placePiece(squareIndex)
+    checkForWinner()
+    checkForTie()
+    switchPlayerTurn()
+    render()
+}
+function placePiece(index){
+    board[index] = turn
+    console.log(board)
+}
+function checkForWinner(){
+    winningCombos.forEach(winningCombo=> {
+        if ( board[winningCombo[0]] !=='' //not empty
+        && board[winningCombo[0]] === board[winningCombo[1]] //1=2
+        && board[winningCombo[0]] === board[winningCombo[2]])//1=3
+            winner = true
+            console.log(winner)
+    })
+}
+function checkForTie(){
+    if (winner) return
+    tie = true
+    board.forEach((square)=>{
+        if (square==='') tie = false
+    })
+}
+function switchPlayerTurn(){
+    if (winner) return
+    else {
+       if (turn==='X') turn = 'O'
+       else if (turn === 'O') turn = 'X' 
+    }
+}
+
+init()
 /*----------------------------- Event Listeners -----------------------------*/
-
-boardElement.addEventListener('click', markSquare)
-buttonElement.addEventListener("click",playagain)
-
-}
-while (play)
+boardEl.addEventListener('click',handleClick)
+resetBtnEl.addEventListener('click',init)
 
